c5a5dd6b5f484df0e97ccbceb6caa41334fe06f2
mari.mirosh <mari.mirosh@bk.ru>
2020-02-17 23:58:22 +0300

    done hw_1_ex_4

---
+++ Homework_1/hw_1_ex_4/hw_1_ex_4/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace hw_1_ex_4.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("hw_1_ex_4")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("HP")>]
+[<assembly: AssemblyProduct("hw_1_ex_4")>]
+[<assembly: AssemblyCopyright("Copyright © HP 2020")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("813128f7-be3a-46a0-8e46-a3afe91b566e")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
