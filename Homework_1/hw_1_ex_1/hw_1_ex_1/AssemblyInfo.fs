5b8cd8d7fa748755d3620c69ee858ac357ecbbd0
mari.mirosh <mari.mirosh@bk.ru>
2020-02-17 23:46:37 +0300

    done hw_1_ex_1

---
+++ Homework_1/hw_1_ex_1/hw_1_ex_1/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace hw_1_ex_1.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("hw_1_ex_1")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("HP")>]
+[<assembly: AssemblyProduct("hw_1_ex_1")>]
+[<assembly: AssemblyCopyright("Copyright © HP 2020")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("71a42a5e-b55a-435e-8d81-e9ae2c2169fa")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Homework_1/hw_1_ex_1/hw_1_ex_1/Program.fs
@@ -0,0 +1,15 @@
+﻿module Program
+
+let rec factorial x =
+    if x <= 1
+    then 1
+    else
+        let resultOfRec = factorial(x - 1)
+        let result = x * resultOfRec
+        result
+
+let test =
+    let testData = [0; 1; 2; 3; 5]
+    let testAnswers = [1; 1; 2; 6; 120]
+    for i in 0 .. testData.Length - 1 do
+        assert (testAnswers.Item(i) = factorial(testData.Item(i)))
