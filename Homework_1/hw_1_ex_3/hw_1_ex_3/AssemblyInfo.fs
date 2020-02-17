054fcf50a8d21688640532fd1ad27128d92dc31b
mari.mirosh <mari.mirosh@bk.ru>
2020-02-17 23:51:24 +0300

    done hw_1_ex_3

---
+++ Homework_1/hw_1_ex_3/hw_1_ex_3/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace hw_1_ex_3.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("hw_1_ex_3")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("HP")>]
+[<assembly: AssemblyProduct("hw_1_ex_3")>]
+[<assembly: AssemblyCopyright("Copyright © HP 2020")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("f8baa9ec-2c05-4ae1-b256-03b998677059")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Homework_1/hw_1_ex_3/hw_1_ex_3/Program.fs
@@ -0,0 +1,16 @@
+﻿module Program
+
+let listRevolution (list:list<_>) =
+    let mutable newList = []
+    for i in 0 .. list.Length - 1 do
+        newList <- list.Item(i) :: newList
+    newList
+
+let test =
+    let testList1 = [1; 2; 3; 4; 5]
+    let testAnswer1 = [5; 4; 3; 2; 1]
+    let testList2 = []
+    let testList3 = [1]
+    assert (testAnswer1.Equals(listRevolution(testList1)))
+    assert (testList2.Equals(listRevolution(testList2)))
+    assert (testList3.Equals(listRevolution(testList3)))
