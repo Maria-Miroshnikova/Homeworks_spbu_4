0c2dd7a1be6e24cb8387c4aae056e63679bf27c7
mari.mirosh <mari.mirosh@bk.ru>
2020-02-18 00:02:29 +0300

    change n .. m to n .. n + m in makingPowerList

--- Homework_1/hw_1_ex_4/hw_1_ex_4/Program.fs
+++ Homework_1/hw_1_ex_4/hw_1_ex_4/Program.fs
@@ -1,11 +1,11 @@
 ﻿module Program
 
 let makingPowerList (n, m) =
-    let (list:list<float>) = [for i in n .. m -> 2.0 ** i]
+    let (list:list<float>) = [for i in n .. n + m -> 2.0 ** i]
     list
 
 let test = 
-    let testData = [(1.0, 1.0); (0.0, 5.0); (-2.0, 2.0); (1.0, -1.0)]
+    let testData = [(1.0, 0.0); (0.0, 5.0); (-2.0, 4.0); (4.0, -2.0)]
     let testAnswer = [[2.0]; [1.0; 2.0; 4.0; 8.0; 16.0; 32.0]; [0.25; 0.5; 1.0; 2.0; 4.0]; []]
     for i in 0 .. testData.Length - 1 do
         assert (testAnswer.Item(i).Equals(makingPowerList(testData.Item(i))))
